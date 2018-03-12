008116127b2133c33f07d18dd59252fec63304f8
Nah <bj137759@students.davenport.k12.ia.us>
2018-03-12 14:50:37 -0500

    trying something new

---
+++ finalGameWork/finalGameWork/classesAndStorylines/knightWork/knightStoryCastle.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace gameStart
+{
+    partial class knightStoryCastle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblCastleStart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go in through the front door";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Make your way around to the back";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblCastleStart
+            // 
+            this.lblCastleStart.AutoSize = true;
+            this.lblCastleStart.Location = new System.Drawing.Point(424, 222);
+            this.lblCastleStart.Name = "lblCastleStart";
+            this.lblCastleStart.Size = new System.Drawing.Size(0, 13);
+            this.lblCastleStart.TabIndex = 2;
+            // 
+            // knightStoryCastle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gameStart.Properties.Resources.castleStart;
+            this.ClientSize = new System.Drawing.Size(969, 515);
+            this.Controls.Add(this.lblCastleStart);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "knightStoryCastle";
+            this.Text = "knightStoryCastle";
+            this.Load += new System.EventHandler(this.knightStoryCastle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblCastleStart;
+    }
+}
---
+++ finalGameWork/finalGameWork/classesAndStorylines/knightWork/locationStartKnight.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace gameStart
+{
+    partial class locationStartKnight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCastleStart = new System.Windows.Forms.Button();
+            this.btnForestStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCastleStart
+            // 
+            this.btnCastleStart.Location = new System.Drawing.Point(610, 284);
+            this.btnCastleStart.Name = "btnCastleStart";
+            this.btnCastleStart.Size = new System.Drawing.Size(104, 40);
+            this.btnCastleStart.TabIndex = 18;
+            this.btnCastleStart.Text = "Castle(Hard)";
+            this.btnCastleStart.UseVisualStyleBackColor = true;
+            this.btnCastleStart.Click += new System.EventHandler(this.btnCastleStart_Click);
+            // 
+            // btnForestStart
+            // 
+            this.btnForestStart.Location = new System.Drawing.Point(98, 283);
+            this.btnForestStart.Name = "btnForestStart";
+            this.btnForestStart.Size = new System.Drawing.Size(109, 41);
+            this.btnForestStart.TabIndex = 17;
+            this.btnForestStart.Text = "Forest(Easy)";
+            this.btnForestStart.UseVisualStyleBackColor = true;
+            this.btnForestStart.Click += new System.EventHandler(this.btnForestStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Choose your start location";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::gameStart.Properties.Resources.castleStart;
+            this.pictureBox6.Location = new System.Drawing.Point(512, 139);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(294, 138);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 16;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::gameStart.Properties.Resources.forestStart;
+            this.pictureBox5.Location = new System.Drawing.Point(22, 139);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(276, 138);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // locationStartKnight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 462);
+            this.Controls.Add(this.btnCastleStart);
+            this.Controls.Add(this.btnForestStart);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label2);
+            this.Name = "locationStartKnight";
+            this.Text = "locationStart";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCastleStart;
+        private System.Windows.Forms.Button btnForestStart;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+    }
+}
